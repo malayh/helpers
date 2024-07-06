6f2c5035cfd57ca13121f26e346456705e6ffe8a
Malay <malay.hazarika@gmail.com>
2024-07-06 16:19:53 +0530

    Fix w

--- misc.bash
+++ misc.bash
@@ -1,9 +1,11 @@
 inithelprs() { source "$HOME/.bashrc"; }
 
 w() {
+
     while true; do
+        $@ > /tmp/w.out;    
         tput clear;
-        $@;
+        cat /tmp/w.out;
         sleep 1;
     done
 }
